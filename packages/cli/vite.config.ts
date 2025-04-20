04ba28eb26674b6ab09fcc054648d03f812db216
Adam Finlayson <amf@adamfinlayson.com>
2025-04-20 13:16:20 -0500

    chore: config: build

--- packages/cli/vite.config.ts
+++ packages/cli/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format, entryName) => `${entryName}.js`,
     },
     rollupOptions: {
-      external: ["commander"],
+      external: ["commander", "enquirer", "fs", "path"],
     },
   },
 });
